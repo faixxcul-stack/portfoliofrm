13b1e912110b70f5bcf4afe07477d87d180e0e78
fikri <faixxcul@gmail.com>
2025-10-05 14:47:45 +0700

    first init github

---
+++ script.js
@@ -0,0 +1,25 @@
+// type animation
+var typed = new Typed("#typed", {
+    strings: ["Finance Analyst", "Undergraduate Business Management"],
+    typeSpeed: 50,
+    backSpeed: 20,
+    backDelay: 3000,
+    showCursor: false,
+    loop: true
+});
+
+// Theme
+function theme() {
+    const darkBtn = document.getElementById('darkBtn');
+    const lightBtn = document.getElementById('lightBtn');
+
+    document.body.classList.toggle('light');
+
+    if (document.body.classList.contains('light')) {
+        darkBtn.style.display = 'block';
+        lightBtn.style.display = 'none';
+    } else {
+        darkBtn.style.display = 'none';
+        lightBtn.style.display = 'block';
+    }
+}
